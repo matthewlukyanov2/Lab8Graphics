f99a22e4d462cf0c38bac36ecc20a7caa29b3b44
MATTHEW LUKYANOV - STUDENT <g00421514@atu.ie>
2024-11-22 10:32:10 +0000

    added obel and sobel horizontal lines

--- lab8.py
+++ lab8.py
@@ -5,25 +5,33 @@ from matplotlib import pyplot as plt
 imgOrig = cv2.imread('ATU.jpg')  
 imgGray = cv2.cvtColor(imgOrig, cv2.COLOR_BGR2GRAY)
 
-imgOut1 = cv2.GaussianBlur(imgOrig, (15, 15), 0)  
-imgOut2 = cv2.GaussianBlur(imgOrig, (25, 25), 0)  
+obelHorizontal = cv2.Sobel(imgGray, cv2.CV_64F, 1, 0, ksize=5)  
+sobelVertical = cv2.Sobel(imgGray, cv2.CV_64F, 0, 1, ksize=5)
 
-plt.subplot(2, 2, 1)
+imgOut1 = cv2.GaussianBlur(imgOrig, (15, 15), 0)  
+imgOut2 = cv2.GaussianBlur(imgOrig, (25, 25), 0) 
+
+nrows = 2
+ncols = 2
+
+plt.figure(figsize=(10, 5))
+
+plt.subplot(nrows, ncols, 1)
 plt.imshow(cv2.cvtColor(imgOrig, cv2.COLOR_BGR2RGB))  
 plt.title('Original Image')
 plt.xticks([]), plt.yticks([]) 
 
-plt.subplot(2, 2, 2)
+plt.subplot(nrows, ncols, 2)
 plt.imshow(imgGray, cmap='gray') 
 plt.title('Grayscale Image')
 plt.xticks([]), plt.yticks([]) 
 
-plt.subplot(2, 2, 3)
+plt.subplot(nrows, ncols, 3)
 plt.imshow(cv2.cvtColor(imgOut1, cv2.COLOR_BGR2RGB))  
 plt.title('GaussianBlur (15x15)')
 plt.xticks([]), plt.yticks([]) 
 
-plt.subplot(2, 2, 4)
+plt.subplot(nrows, ncols, 4)
 plt.imshow(cv2.cvtColor(imgOut2, cv2.COLOR_BGR2RGB))  
 plt.title('GaussianBlur (25x25)')
 plt.xticks([]), plt.yticks([]) 
